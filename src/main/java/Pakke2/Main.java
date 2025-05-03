c5134180cadcee39d5e834530954d0ffb39137e6
jawadaf <jafa0001@stud.kea.dk>
2025-05-03 16:57:00 +0200

    Update: added a function - who will start

--- src/main/java/Pakke2/Main.java
+++ src/main/java/Pakke2/Main.java
@@ -3,6 +3,7 @@ package Pakke2;
 public class Main {
     public static void main(String[] args) {
         Board.initializeBoard();
+        Rules.askIfPlayerStarts(); // Spørg om du vil starte
 
         while (true) {
             Board.displayBoard();
